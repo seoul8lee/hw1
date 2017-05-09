8b4ad557f3869d0712e1e4c35bb16c0b0a7bb344
HYUNKYUNG LEE <seoul8lee@likelion.org>
2017-05-10 01:35:36 +0900

    crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end 
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(params[:post].permit(:title, :body))
+		@post.save
+		redirect_to @post
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(params.requre(:post).permit(:title, :body))
+		redirect_to @post
+	end 
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end 
+end
